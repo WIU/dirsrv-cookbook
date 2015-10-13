05ac5f98025066ef9d535eeb8177978049d7c3b0
Matt Mencel <matt@techminer.net>
2015-10-13 12:31:29 -0500

    ass approximate to index

--- resources/index.rb
+++ resources/index.rb
@@ -26,6 +26,7 @@ attribute :database, :kind_of => String, :default => 'userRoot'
 attribute :equality, :kind_of => [ TrueClass, FalseClass ], :default => true
 attribute :presence, :kind_of => [ TrueClass, FalseClass ], :default => false
 attribute :substring, :kind_of => [ TrueClass, FalseClass ], :default => false
+attribute :approximate, :kind_of => [ TrueClass, FalseClass ], :default => false
 attribute :host, :kind_of => String, :default => 'localhost'
 attribute :port, :kind_of => Integer, :default => 389
 attribute :credentials, :kind_of => [ String, Hash ], :default => 'default_credentials'
